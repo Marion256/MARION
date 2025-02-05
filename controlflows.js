f413c12266fefdcc2703bef6644b0953b97aeb27
Marion256 <mugabimarion618@gmail.com>
2025-02-05 11:47:52 +0300

    Add files via upload

---
+++ controlflows.js
@@ -0,0 +1,45 @@
+/**let dayOfWeek =4
+switch(dayOfWeek){
+    case 1:
+        console.log("Monday")
+break;
+
+case 2:
+    console.log("Tuesday")
+break;
+
+case 3:
+    console.log("Wendensday")
+break;
+
+case 4:
+    console.log("Thursday")
+break;
+
+case 5:
+    console.log("Friday")
+break;
+
+case 6:
+    console.log("saturday")
+break;
+
+case 7:
+    console.log("sunday")
+
+break;*/
+
+
+
+
+//fro loop.
+//a program that can print the first 100 counting numbers.
+// *(let i=0;i<=100;i++){
+   *// console.log(i);*//
+}
+// while loop.
+let(i)=0;
+while(i<=100){
+    console.log(i);
+    i++;
+} 
